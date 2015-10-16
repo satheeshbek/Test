dc36ce1bccc630c5476c79e3ae41b6e4a598f0b4
Test <Test@Satheeshs-MacBook-Pro.local>
2015-10-16 15:53:08 +0530

    Initial Commit

---
+++ teup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  teup
+//
+//  Created by Test on 10/16/15.
+//  Copyright (c) 2015 Oneteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ teup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teup
+//
+//  Created by Test on 10/16/15.
+//  Copyright (c) 2015 Oneteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
